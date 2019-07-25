047bc9015cdefa6afbd5fe368fccc713c25d7e34
Marco Spanhaak <43254817+Spanhaak@users.noreply.github.com>
2019-07-25 08:12:18 +0200

    added JS code to fix the logo when responsive

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  navbarLinks.classList.toggle('active'),
+  document.getElementsByClassName('content')[0].style.marginTop = '120px';
 })

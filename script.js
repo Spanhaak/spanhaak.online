5b0d2bc41d9b3d60ccd14681bbe9ec5cd0616962
Marco Spanhaak <43254817+Spanhaak@users.noreply.github.com>
2019-07-25 12:07:17 +0200

    Changed CSS to reflect menu choices

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active'),
   document.getElementsByClassName('content')[0].style.marginTop = '120px';
